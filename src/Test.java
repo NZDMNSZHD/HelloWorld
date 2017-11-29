8fdf245007decc5c01b9766d1952b5cef264c1f4
limingfeng <limingfeng@xinzhentech.com>
2017-11-29 16:04:56 +0800

    no message

--- src/Test.java
+++ src/Test.java
@@ -2,11 +2,11 @@ public class Test {
     public static void main(String[] args) {
         System.out.println(10);
         System.out.println("难道真的是我想的那样，会发生冲突吗？");
-        System.out.println("难道真的是我想的那样，会发生冲突吗？");
+        System.out.println("我不知道啊！");
 
-        System.out.println("难道真的是我想的那样，会发生冲突吗？");
-		System.out.println("难道真的是我想的那样，会发生冲突吗？");
-        System.out.println("难道真的是我想的那样，会发生冲突吗？");
+        System.out.println("你怎么可能不知道呢？");
+		System.out.println("我一定要知道吗？");
+        System.out.println("是的！");
 
         System.out.println("已修改一条输出语句！");
     }
