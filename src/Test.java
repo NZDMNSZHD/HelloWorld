4d4e3280a92b52825a9bcd9350e89eac4db459ec
limingfeng <limingfeng@xinzhentech.com>
2017-11-29 15:59:48 +0800

    no message

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,7 @@ public class Test {
         System.out.println("难道真的是我想的那样，会发生冲突吗？");
         System.out.println("难道真的是我想的那样，会发生冲突吗？");
         System.out.println("难道真的是我想的那样，会发生冲突吗？");
+		System.out.println("难道真的是我想的那样，会发生冲突吗？");
+        System.out.println("难道真的是我想的那样，会发生冲突吗？");
     }
 }
