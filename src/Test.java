1c32f380e84a870172fdc34082c96364b3f349f9
limingfeng <limingfeng@xinzhentech.com>
2017-11-29 16:34:33 +0800

    no message

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,7 @@ public class Test {
         System.out.println("难道真的是我想的那样，会发生冲突吗？");
 
         System.out.println("设想1：未拉取就先修改，修改完后再提交，这样会发生冲突？");
+        System.out.println("经过测试，设想1成立，会发生冲突！SourceTree拉取和推送失败！");
 
 
 
